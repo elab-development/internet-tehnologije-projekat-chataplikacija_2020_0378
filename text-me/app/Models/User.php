627d42ee66bfa176152b13b2f666cfa49873f46e
BogdanZivkovic2001 <bogdanzv2001@gmail.com>
2024-07-14 16:09:34 +0200

    Izmene u modelima
    Dodavanje protected fillable svim modelima

--- text-me/app/Models/User.php
+++ text-me/app/Models/User.php
@@ -17,9 +17,12 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
+        'avatar',
         'name',
         'email',
+        'email_verified_at',
         'password',
+        'is_admin'
     ];
 
     /**
