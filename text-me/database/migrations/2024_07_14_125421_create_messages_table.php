ee9a2510f538121299b6c6ad4a5f03c5dec24ce6
BogdanZivkovic2001 <bogdanzv2001@gmail.com>
2024-07-14 15:51:40 +0200

    Promene u tabeli messages

--- text-me/database/migrations/2024_07_14_125421_create_messages_table.php
+++ text-me/database/migrations/2024_07_14_125421_create_messages_table.php
@@ -7,14 +7,28 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration
 {
     /**
-     * Run the migrations.
+     * Run the migrationsw.
      */
     public function up(): void
     {
         Schema::create('messages', function (Blueprint $table) {
             $table->id();
+            $table->longText('message')->nullable();//stavljamo nullable jer ponekad zelimo da posaljemo atachment bez poruke
+            $table->foreignId('sender_id')->constrained('users');
+            $table->foreignId('receiver_id')->nullable()->constrained('users');//stavljamo da je nullable jer poruka ne mora stici samo jednom korisniku
+            $table->foreignId('group_id')->nullable()->constrained('groups');// mora postojti ili group_id ili receiver_id da bi poruka bila validna
+            $table->foreignId('conversation_id')->nullable()->constrained('conversations');
             $table->timestamps();
         });
+
+        Schema::create('groups', function (Blueprint $table){
+            $table->foreignId('last_message_id')->nullable()->constrained('messages');//u tabeli grupa dodajemo kolonu last message id
+        });
+
+        Schema::create('conversations', function (Blueprint $table){
+            $table->foreignId('last_message_id')->nullable()->constrained('messages');
+        });
+
     }
 
     /**
