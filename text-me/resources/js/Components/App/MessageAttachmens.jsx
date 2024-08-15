0a3f8fee73acffb3f478ba275c3b01772b80cca1
Aleksandra <maleksandrabiz@gmail.com>
2024-08-15 18:34:42 +0200

    Notifikacije za pristigle poruke

--- text-me/resources/js/Components/App/MessageAttachmens.jsx
+++ text-me/resources/js/Components/App/MessageAttachmens.jsx
@@ -18,7 +18,7 @@ const MessageAttachments = ({attachments, attachmentClick}) => {
                                     ? "w-84"
                                     : "w-32 aspect-square bg-blue-100")
                             }
-                        >
+                        > 
                             {!isAudio(attachment) && (
                                 <a
                                     onClick={(ev) => ev.stopPropagation()}
@@ -26,8 +26,7 @@ const MessageAttachments = ({attachments, attachmentClick}) => {
                                     href={attachment.url}
                                     className="z-20 opacity-100 group-hover:opacity-100
                                     transition-all w-8 h-8 flex items-center justify-center
-                                    text-gray-100 bg-gray-700 rounded absolute right-0 top-0
-                                    cursor-pointer hover:bg-gray-800"
+                                    text-gray-100 bg-gray-700 rounded cursor-pointer hover:bg-gray-800 absolute right-0 top-0  "
                                 >
                                     <ArrowDownTrayIcon className="w-4 h-4"/>
                                 </a>
